bc457f5ab688ca4e37ac8200c0451c53db1d314e
binaryants <18505585901@163.com>
2022-06-28 16:11:29 +0800

    测试2

--- tuling-admin/src/main/java/com/tulingxueyuan/mall/StartApp.java
+++ tuling-admin/src/main/java/com/tulingxueyuan/mall/StartApp.java
@@ -14,7 +14,6 @@ public class StartApp {
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
         //System.out.println(BCrypt.hashpw("123456"));
-        //测试github
     }
 
 }

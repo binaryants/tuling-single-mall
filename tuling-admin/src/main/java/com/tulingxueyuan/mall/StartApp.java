66d2afbc87bfb36d5b44fbf27427d74dfd0c909e
binaryants <18505585901@163.com>
2022-07-06 20:22:10 +0800

    2022.07.06测试

--- tuling-admin/src/main/java/com/tulingxueyuan/mall/StartApp.java
+++ tuling-admin/src/main/java/com/tulingxueyuan/mall/StartApp.java
@@ -5,8 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /***
- * @Author 徐庶   QQ:1092002729
- * @Slogan 致敬大师，致敬未来的你
+ * @Author Bin
  */
 @SpringBootApplication
 public class StartApp {
